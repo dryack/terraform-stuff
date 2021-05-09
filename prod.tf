a3bbf87e96f26c4132c7e05b2cb39a926fa6391d
dryack <lamashtu@wait-wtf.com>
2021-05-09 12:50:51 -0700

    Adding VPC

--- prod.tf
+++ prod.tf
@@ -3,7 +3,9 @@ provider "aws" {
     region = "us-west-2"
 }   
 
-resource "aws_s3_bucket" "tf_class" {
+resource "aws_s3_bucket" "prod_tf_class" {
     bucket  = "dryack-tf-class-2021"
     acl     = "private"
 }
+
+resource "aws_default_vpc" "default" {}
