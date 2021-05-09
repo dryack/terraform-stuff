16d6d9ab020430c3af71dc071f8f28d7ef93e48b
dryack <lamashtu@wait-wtf.com>
2021-05-09 12:58:32 -0700

    Added web security group

--- prod.tf
+++ prod.tf
@@ -9,3 +9,31 @@ resource "aws_s3_bucket" "prod_tf_class" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+    name        = "prod_web"
+    description = "Allow standard http/https ports inbound and everything outbound"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["23.124.108.20/32", "149.56.26.83/32"]
+    }
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["23.124.108.20/32", "149.56.26.83/32"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = -1
+        cidr_blocks = ["23.124.108.20/32", "149.56.26.83/32"]
+    }
+
+    tags = {
+        "Terrform" : "true"
+    }
+}
