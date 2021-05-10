450ddd44f4bdb88e692b78506f0699aecd4a0776
dryack <lamashtu@wait-wtf.com>
2021-05-10 14:31:17 -0700

    Auto scaling group added and working

--- prod.tf
+++ prod.tf
@@ -81,46 +81,14 @@ resource "aws_security_group" "prod_elb" {
     }
 }
 
-#resource "aws_ami_launch_permission" "prod_web" {
-#    image_id    = "ami-03e7d2d88e3e9de77"
-#    account_id  = "465478892372"
-#}
-
 variable "azs_array" {
     type    = list(string)
     default = ["us-west-2a", "us-west-2c"]
 }
 
-resource "aws_instance" "prod_web" {
-    count = 2
-
-    ami                 = "ami-03e7d2d88e3e9de77"
-    instance_type       = "t2.nano"
-    availability_zone   = var.azs_array[count.index]
-
-    vpc_security_group_ids = [
-        aws_security_group.prod_web.id
-    ]
-    
-    tags = {
-        "Terraform" : "true"
-    }
-}
-
-resource "aws_eip_association" "prod_web" {
-    instance_id     = aws_instance.prod_web.0.id
-    allocation_id   = aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
-    tags = {
-        "Terraform" : "true"
-    }
-}
-
 resource "aws_elb" "prod_web" {
     name            = "prod-web"
-    instances       = aws_instance.prod_web.*.id
+    #instances       = aws_instance.prod_web.*.id
     subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
     #security_groups = [aws_security_group.prod_web.id]
     security_groups = [aws_security_group.prod_elb.id]
@@ -135,4 +103,39 @@ resource "aws_elb" "prod_web" {
     tags = {
         "Terraform" : "true"
     }
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-03e7d2d88e3e9de77"
+  instance_type = "t2.nano"
+  vpc_security_group_ids = [aws_security_group.prod_web.id]
+
+    tags = {
+        "Terraform" : "true"
+    }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  #availability_zones    = ["us-west-2a","us-west-2c"]
+  vpc_zone_identifier   = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity      = 2
+  max_size              = 2
+  min_size              = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = "true"
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
