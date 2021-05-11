c4d4a15c699759cb580d0ca6a062fa2ab26bcb69
dryack <lamashtu@wait-wtf.com>
2021-05-10 21:41:14 -0700

    Broke out prod into web_app module

---
+++ modules/web_app/variables.tf
@@ -0,0 +1,29 @@
+variable "web_image_id" {
+    type = string
+}
+variable "web_instance_type" {
+    type = string
+}
+variable "web_desired_capacity" {
+    type = number
+}
+variable "web_max_size" {
+    type = number
+}
+variable "web_min_size" {
+    type = number
+}
+
+variable "subnets" {
+    type = list(string)
+}
+
+variable "security_groups_web" {
+    type = list(string)
+}
+variable "security_groups_elb" {
+    type = list(string)
+}
+variable "web_app" {
+    type = string
+}
--- prod.tf
+++ prod.tf
@@ -85,10 +85,9 @@ resource "aws_security_group" "prod_web" {
     }
 }
 
-# The tutorial didn't cover this SG, but it immediately solved the issue
 resource "aws_security_group" "prod_elb" {
     name        = "prod_elb"
-    description = "Allow ELB to accept incoming connections via http/https"
+    description = "Allow standard http/https ports inbound and everything outbound"
 
     ingress {
         from_port   = 80
@@ -100,7 +99,7 @@ resource "aws_security_group" "prod_elb" {
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-       #cidr_blocks = var.web_ext_incoming_allowlist
+        cidr_blocks = var.web_ext_incoming_allowlist
     }
     egress {
         from_port   = 0
@@ -114,56 +113,16 @@ resource "aws_security_group" "prod_elb" {
     }
 }
 
-resource "aws_elb" "prod_web" {
-    name            = "prod-web"
-    #instances       = aws_instance.prod_web.*.id
-    subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-    #security_groups = [aws_security_group.prod_web.id]
-    security_groups = [aws_security_group.prod_elb.id]
+module "web_app" {
+    source = "./modules/web_app"
 
-    listener {
-        instance_port       = 80
-        instance_protocol   = "http"
-        lb_port             = 80
-        lb_protocol         = "http"
-    }
-    
-    tags = {
-        "Terraform" : "true"
-    }
-}
-
-resource "aws_launch_template" "prod_web" {
-  name_prefix   = "prod-web"
-  image_id      = var.web_image_id
-  instance_type = var.web_instance_type
-  vpc_security_group_ids = [aws_security_group.prod_web.id]
-
-    tags = {
-        "Terraform" : "true"
-    }
-}
-
-resource "aws_autoscaling_group" "prod_web" {
-  #availability_zones    = ["us-west-2a","us-west-2c"]
-  vpc_zone_identifier   = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-  desired_capacity      = var.web_desired_capacity
-  max_size              = var.web_max_size
-  min_size              = var.web_min_size
-
-  launch_template {
-    id      = aws_launch_template.prod_web.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Terraform"
-    value               = "true"
-    propagate_at_launch = "true"
-  }
-}
-
-resource "aws_autoscaling_attachment" "prod_web" {
-  autoscaling_group_name = aws_autoscaling_group.prod_web.id
-  elb                    = aws_elb.prod_web.id
+    web_image_id            = var.web_image_id
+    web_instance_type       = var.web_instance_type
+    web_desired_capacity    = var.web_desired_capacity
+    web_max_size            = var.web_max_size
+    web_min_size            = var.web_min_size
+    subnets                 = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+    security_groups_web     = [aws_security_group.prod_web.id]
+    security_groups_elb     = [aws_security_group.prod_elb.id]
+    web_app                 = "prod"
 }
